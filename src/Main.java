c05764f21481bb81018696aaa59a1f4c90c8a1f1
Ohad <ohadlevin99@gmail.com>
2021-11-12 10:32:31 +0200

    Stage one Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String... args){
-        System.out.println("Welcome To Mט Horrendous Gama Project!\nEnjoy Hell!!!");
+    public static void main(String[] args){
+        int cost;
+        String meat = System.getProperty("meat");
+        String bread = System.getProperty("bread");
+
+        if(meat.equals("meat")) cost = 50;
+        else if (meat.equals("lamb")) cost = 55;
+        else cost = 60;
+
+        System.out.println("Hamburger Order:\nCost:" + cost + "\n" + meat + "\n" + bread);
+        for(String arg : args) {
+            System.out.println(arg);
+        }
+
+
+
     }
 }
