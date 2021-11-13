7b1504858fe01e796ebfd832d24547ce31a857e4
Ohad <ohadlevin99@gmail.com>
2021-11-13 21:10:37 +0200

    semi stable version - no editing extras

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Order order = new Order();
 
-        Scanner in = new Scanner(System.in);
-        int cost = 0;
-        boolean exists = true;
-
-        System.out.println("Enter Burger Meat:");
-        String meat = in.next();
-
-        System.out.println("Enter Burger Bread:");
-        String bread = in.next();
-        in.nextLine();
-
-        System.out.println("Enter Misc:");
-        String misc = in.nextLine();
-        String[] veggies = misc.split("\\W+");
-        for(String veggie : veggies) {
-            if(!veggie.equals("cucumber") && !veggie.equals("tomato") && !veggie.equals("onion") && !veggie.equals("pickles") && !veggie.equals("lettuce")) exists = false;
-        }
-
-
-
-        switch (meat) {
-            case "cow":
-                cost = 50;
-                break;
-            case "lamb":
-                cost = 55;
-                break;
-            case "tophu":
-                cost = 60;
-                break;
-            default:
-                exists = false;
-        }
-
-        if(!bread.equals("white") && !bread.equals("whole")) exists = false;
-
-        if(exists) System.out.println("Hamburger Order:\nCost:" + cost + "\n" + meat + "\n" + bread + "\n" + Arrays.toString(veggies));
-        else System.out.println("this meal does not exist!");
+        System.out.println(order.getCost());
+        System.out.println(order.isValid());
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,8 +7,9 @@ public class Order {
     private Scanner input;
     private String meat;
     private String bread;
-    private List<String> extras;
-    private int cost;
+    private HashMap<String, Integer> meats;
+    private HashMap<String, Integer> extras;
+    private List<String> pickedExtras;
 
     public Order() {
         input = new Scanner(System.in);
@@ -21,12 +23,44 @@ public class Order {
 
         System.out.println("Enter Misc:");
         String extrasRaw = input.nextLine();
-        extras = Arrays.asList(extrasRaw.split("\\W+"));
+        pickedExtras = Arrays.asList(extrasRaw.split("\\W+"));
+
+        extras = new HashMap<String, Integer>();
+        extras.put("onion", 50);
+        extras.put("cucumber", 55);
+        extras.put("tomato", 60);
+        extras.put("lettuce", 60);
+        extras.put("pickles", 60);
+
+
+        meats = new HashMap<String, Integer>();
+        meats.put("cow", 50);
+        meats.put("lamb", 55);
+        meats.put("tophu", 60);
     }
 
-    private boolean isValid(){
-        boolean valid = true;
-        extras.forEach();
-        return valid;
+    public boolean isValid(){
+        for(String extra : pickedExtras){
+            if(!extras.containsKey(extra)) return false;
+        }
+        if(!bread.equals("white") && !bread.equals("whole")) return false;
+        if(!meats.containsKey(meat)) return false;
+        return true;
+    }
+
+    public int getCost(){
+        int extraCost = 0;
+        for(String extra : pickedExtras){
+            extraCost += extras.get(extra);
+        }
+        return meats.get(meat) + extraCost;
+    }
+
+    public void addExtra(String extra, Integer cost){
+        extras.put(extra, cost);
+    }
+
+    public String toString(){
+        return "This hamburger order contains:\n" + meat + "\n" + bread + "\n" + Arrays.toString(pickedExtras.toArray()) + "\nCost:" + getCost();
     }
 }
